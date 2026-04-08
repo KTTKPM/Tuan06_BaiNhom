f340015bf8e49b09d680ee39709c397a7639f8a4
H82M19\Student <Student>
2026-04-08 20:12:07 +0700

    update order service basic

--- src/dto/userDto.js
+++
@@ -1,9 +0,0 @@
-class UserResponseDTO {
-    constructor(user) {
-        this.id = user.id;
-        this.username = user.username;
-        this.role = user.role;
-    }
-}
-
-module.exports = { UserResponseDTO };
